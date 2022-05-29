7f4919d105f74fd3bd9dc8cf4d458eda6133f5c2
CleberBatista36 <cleberbatista.silva@gmail.com>
2022-05-29 14:12:31 -0300

    primeiros arquivos

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero.model';
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Jesus'},
+  {id: 2, name: 'Mamãe'},
+  {id: 3, name: 'Calrla'},
+  {id: 4, name: 'Andrea'},
+  {id: 5, name: 'Jose'},
+  {id: 4, name: 'Maria'},
+  {id: 4, name: 'Roberto'},
+  {id: 4, name: 'Leandro'},
+
+];
